6a717190a283856f5b9e3990f2798d151c4f5c17
HenningCode <henningwilmer@yahoo.de>
2024-02-18 22:28:30 +0100

    readd zsh

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -70,6 +70,8 @@
     jack.enable = true;
   };
 
+  programs.zsh.enable = true;
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.henning = {
     isNormalUser = true;
