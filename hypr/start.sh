4d6945a6780a250a6c692edf59dd262086f62036
HenningCode <henningwilmer@yahoo.de>
2024-02-22 19:44:00 +0100

    Stage hypr

---
+++ hypr/start.sh
@@ -0,0 +1,9 @@
+
+swww init &
+swww img ~/Wallpapers/gruvbox-cat_march.png &
+
+nm-applet --indicator &
+
+waybar &
+
+dunst $
--- modules/home-manager/wlogout.nix
+++ modules/home-manager/wlogout.nix
@@ -1,26 +1,87 @@
 { pkgs, user, theme, ... }:
-
 {
-
-  home.packages = [ powerMenu ];
   programs.wlogout = {
     enable = true;
     layout = [
       {
-        "label" = "lock";
-        "action" = "gtklock";
-        "keybind" = "l";
+        "label" = "logout";
+        "action" = "hyprctl dispatch exit 0";
+        "text" = "        ";
       }
+
       {
-        "label" = "reboot";
-        "action" = "reboot";
-        "keybind" = "r";
-      } 
+        "label" = "lock";
+        "action" = "swaylock";
+        "text" = "        ";
+      }
+
+
       {
         "label" = "shutdown";
-        "action" = "poweroff";
-        "keybind" = "s";
+        "action" = "systemctl poweroff";
+        "text" = "        ";
+      }
+
+      {
+        "label" = "reboot";
+        "action" = "systemctl reboot";
+        "text" = "        ";
+      }
+
+      {
+        "label" = "suspend";
+        "action" = "systemctl suspend";
+        "text" = "        ";
       }
     ];
+    style = ''
+      * {
+          background-image: none;
+        }
+        window {
+            font-family: JetBrains Mono Nerd Font;
+            font-size: 14pt;
+            color: #ffffff; /* text */
+            background-color: rgba(46, 52, 64, 0);
+        }
+        button {
+            background-repeat: no-repeat;
+            background-position: center;
+            background-size: 25%;
+            border: none;
+            background-color: rgba(30, 30, 46, 0);
+            margin: 5px;
+            color: #ffffff;
+            transition: box-shadow 0.2s ease-in-out, background-color 0.2s ease-in-out;
+        }
+        button:focus, button:active, button:hover {
+          background-color: #b9b1e2;
+          outline-style: none;
+        }
+
+        #lock {
+            background-image: image(url("/home/wilf/.config/wlogout/icons/lock.png"), url("/usr/local/share/wlogout/icons/lock.png"));
+        }
+
+        #logout {
+            background-image: image(url("/home/wilf/.config/wlogout/icons/logout.png"), url("/usr/local/share/wlogout/icons/logout.png"));
+        }
+
+        #suspend {
+            background-image: image(url("/home/wilf/.config/wlogout/icons/suspend.png"), url("/usr/local/share/wlogout/icons/suspend.png"));
+        }
+
+        #hibernate {
+            background-image: image(url("/home/wilf/.config/wlogout/icons/hibernate.png"), url("/usr/local/share/wlogout/icons/hibernate.png"));
+        }
+
+        #shutdown {
+            background-image: image(url("/home/wilf/.config/wlogout/icons/shutdown.png"), url("/usr/local/share/wlogout/icons/shutdown.png"));
+        }
+
+        #reboot {
+            background-image: image(url("/home/wilf/.config/wlogout/icons/reboot.png"), url("/usr/local/share/wlogout/icons/reboot.png"));
+        }
+    '';
   };
 }
